11ccb748b4277f74dfc4c4c769d41fced28978be
vagrant <vagrant@vagrant.vm>
2020-10-03 21:47:37 +0000

    Created function to print data, fixed variable index

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,47 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def produce_daily_summary(file, day):
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+    print(day)
+    the_file = open(file)
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
 
-    print("Delivered {} {}s for total of ${}".format(
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print("Delivered  {} {}s for total of ${}".format(
         count, melon, amount))
-the_file.close()
+    
+    the_file.close()
+
+produce_daily_summary("um-deliveries-20140519.txt", "Day 1")
+
+# print("Day 2")
+# the_file = open("um-deliveries-20140520.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print("Delivered {} {}s for total of ${}".format(
+#         count, melon, amount))
+# the_file.close()
 
 
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+# print("Day 3")
+# the_file = open("um-deliveries-20140521.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+#     print("Delivered {} {}s for total of ${}".format(
+#         count, melon, amount))
+# the_file.close()

aa9753255aef4b55c4417d4238e93b755541ee50
vagrant <vagrant@vagrant.vm>
2020-10-03 22:29:08 +0000

    Removed extra comments

--- produce_summary.py
+++ produce_summary.py
@@ -32,31 +32,3 @@ produce_daily_summary("um-deliveries-20140520.txt", "Day 2")
 produce_daily_summary("um-deliveries-20140521.txt", "Day 3")
 #calls function for day three
 
-# print("Day 2")
-# the_file = open("um-deliveries-20140520.txt")
-# for line in the_file:
-#     line = line.rstrip()
-#     words = line.split('|')
-
-#     melon = words[0]
-#     count = words[0]
-#     amount = words[0]
-
-#     print("Delivered {} {}s for total of ${}".format(
-#         count, melon, amount))
-# the_file.close()
-
-
-# print("Day 3")
-# the_file = open("um-deliveries-20140521.txt")
-# for line in the_file:
-#     line = line.rstrip()
-#     words = line.split('|')
-
-#     melon = words[0]
-#     count = words[0]
-#     amount = words[0]
-
-#     print("Delivered {} {}s for total of ${}".format(
-#         count, melon, amount))
-# the_file.close()

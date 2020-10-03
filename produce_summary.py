296fc7b2f05327ccdd408efda25404b54d2c9735
vagrant <vagrant@vagrant.vm>
2020-10-03 22:07:44 +0000

    Added comments to function about functionality of each line

--- produce_summary.py
+++ produce_summary.py
@@ -1,20 +1,30 @@
 def produce_daily_summary(file, day):
+    """Puts melon delivery info from file into daily report""" 
 
     print(day)
+    #prints day of report
     the_file = open(file)
+    #opens file for use in function
     for line in the_file:
+    #iterates over file to separate lines
         line = line.rstrip()
+        #removes extra characters at end of lines
         words = line.split('|')
+        #splits the lines of string for data variable assignment
 
         melon = words[0]
         count = words[1]
         amount = words[2]
+        #assigns valiables to words at specified index
 
         print(f"Delivered  {count} {melon}s for total of ${amount}.")
+        #prints "f" string with variables printed in
     
     the_file.close()
+    #closes file
 
 produce_daily_summary("um-deliveries-20140519.txt", "Day 1")
+#calls function on text report on specified date
 
 # print("Day 2")
 # the_file = open("um-deliveries-20140520.txt")
